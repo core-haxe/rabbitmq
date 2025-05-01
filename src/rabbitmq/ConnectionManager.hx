40936244edbe0e2863efeab936b978635b6cc587
shallowmallow <maud.davout@gmail.com>
2025-05-01 19:31:41 +0200

    Removing connection on closed

--- src/rabbitmq/ConnectionManager.hx
+++ src/rabbitmq/ConnectionManager.hx
@@ -68,6 +68,7 @@ class ConnectionManager {
 
     private function onConnectionClosed(_, connection:Connection) {
         trace(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> onConnectionClosed");
+        _connections.remove(connection.url);
     }
 
     private function onConnectionErrored(error:Any, connection:Connection) {
